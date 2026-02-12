fe35eec19bf88c536c50464a0491620a28c2a3d8
Daniel Vu <da949990@ucf.edu>
2026-02-12 15:47:35 -0500

    Adds function to configure timer

--- lab_3/main.c
+++ lab_3/main.c
@@ -1,5 +1,30 @@
-#include <msp430.h> 
+// Flashing the LED with Timer_A, continuous mode, via polling
+#include <msp430fr6989.h>
+#define redLED BIT0 // Red LED at P1.0
+#define greenLED BIT7 // Green LED at P9.7
 
+
+//**********************************
+// Configures ACLK to 32 KHz crystal
+void config_ACLK_to_32KHz_crystal() {
+    // By default, ACLK runs on LFMODCLK at 5MHz/128 = 39 KHz
+
+    // Reroute pins to LFXIN/LFXOUT functionality
+    PJSEL1 &= ~BIT4;
+    PJSEL0 |= BIT4;
+
+    // Wait until the oscillator fault flags remain cleared
+    CSCTL0 = CSKEY; // Unlock CS registers
+
+    do {
+        CSCTL5 &= ~LFXTOFFG; // Local fault flag
+        SFRIFG1 &= ~OFIFG; // Global fault flag
+    } while((CSCTL5 & LFXTOFFG) != 0);
+
+
+    CSCTL0_H = 0; // Lock CS registers
+    return;
+}
 
 /**
  * main.c
