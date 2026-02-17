4090dddd8c23e429c821d25f1709baf57dde469c
Daniel Vu <daniel.vu.05@Daniels-MacBook-Air.local>
2026-02-16 23:47:59 -0500

    Completes part 1.

---
+++ lab3/Debug/src/MSP430_GUI/GUI_JSON.d
@@ -0,0 +1,87 @@
+# FIXED
+
+src/MSP430_GUI/GUI_JSON.obj: ../src/MSP430_GUI/GUI_JSON.c
+src/MSP430_GUI/GUI_JSON.obj: /Users/daniel.vu.05/gitspaces/embedded-systems/lab3/include/GUI_JSON.h
+src/MSP430_GUI/GUI_JSON.obj: /Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdio.h
+src/MSP430_GUI/GUI_JSON.obj: /Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h
+src/MSP430_GUI/GUI_JSON.obj: /Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h
+src/MSP430_GUI/GUI_JSON.obj: /Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdarg.h
+src/MSP430_GUI/GUI_JSON.obj: /Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_types.h
+src/MSP430_GUI/GUI_JSON.obj: /Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h
+src/MSP430_GUI/GUI_JSON.obj: /Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_types.h
+src/MSP430_GUI/GUI_JSON.obj: /Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdlib.h
+src/MSP430_GUI/GUI_JSON.obj: /Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/string.h
+src/MSP430_GUI/GUI_JSON.obj: /Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/xlocale/_string.h
+src/MSP430_GUI/GUI_JSON.obj: /Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdint.h
+src/MSP430_GUI/GUI_JSON.obj: /Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_stdint40.h
+src/MSP430_GUI/GUI_JSON.obj: /Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/stdint.h
+src/MSP430_GUI/GUI_JSON.obj: /Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_stdint.h
+src/MSP430_GUI/GUI_JSON.obj: /Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_stdint.h
+src/MSP430_GUI/GUI_JSON.obj: /Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdbool.h
+src/MSP430_GUI/GUI_JSON.obj: /Users/daniel.vu.05/gitspaces/embedded-systems/lab3/include/HAL.h
+src/MSP430_GUI/GUI_JSON.obj: /Applications/ti/ccs2041/ccs/ccs_base/msp430/include/msp430.h
+src/MSP430_GUI/GUI_JSON.obj: /Applications/ti/ccs2041/ccs/ccs_base/msp430/include/msp430fr6989.h
+src/MSP430_GUI/GUI_JSON.obj: /Applications/ti/ccs2041/ccs/ccs_base/msp430/include/in430.h
+src/MSP430_GUI/GUI_JSON.obj: /Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+src/MSP430_GUI/GUI_JSON.obj: /Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+src/MSP430_GUI/GUI_JSON.obj: /Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdlib.h
+src/MSP430_GUI/GUI_JSON.obj: /Users/daniel.vu.05/gitspaces/embedded-systems/lab3/include/Config_Common.h
+src/MSP430_GUI/GUI_JSON.obj: ../src/MSP430_GUI/jsmn/jsmn.h
+src/MSP430_GUI/GUI_JSON.obj: /Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stddef.h
+
+../src/MSP430_GUI/GUI_JSON.c:
+
+/Users/daniel.vu.05/gitspaces/embedded-systems/lab3/include/GUI_JSON.h:
+
+/Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdio.h:
+
+/Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h:
+
+/Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h:
+
+/Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdarg.h:
+
+/Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_types.h:
+
+/Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h:
+
+/Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_types.h:
+
+/Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdlib.h:
+
+/Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/string.h:
+
+/Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/xlocale/_string.h:
+
+/Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdint.h:
+
+/Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_stdint40.h:
+
+/Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/stdint.h:
+
+/Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_stdint.h:
+
+/Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_stdint.h:
+
+/Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdbool.h:
+
+/Users/daniel.vu.05/gitspaces/embedded-systems/lab3/include/HAL.h:
+
+/Applications/ti/ccs2041/ccs/ccs_base/msp430/include/msp430.h:
+
+/Applications/ti/ccs2041/ccs/ccs_base/msp430/include/msp430fr6989.h:
+
+/Applications/ti/ccs2041/ccs/ccs_base/msp430/include/in430.h:
+
+/Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+/Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
+/Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdlib.h:
+
+/Users/daniel.vu.05/gitspaces/embedded-systems/lab3/include/Config_Common.h:
+
+../src/MSP430_GUI/jsmn/jsmn.h:
+
+/Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stddef.h:
+
---
+++ lab3/Debug/src/MSP430_GUI/jsmn/jsmn.d
@@ -0,0 +1,18 @@
+# FIXED
+
+src/MSP430_GUI/jsmn/jsmn.obj: ../src/MSP430_GUI/jsmn/jsmn.c
+src/MSP430_GUI/jsmn/jsmn.obj: ../src/MSP430_GUI/jsmn/jsmn.h
+src/MSP430_GUI/jsmn/jsmn.obj: /Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stddef.h
+src/MSP430_GUI/jsmn/jsmn.obj: /Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h
+src/MSP430_GUI/jsmn/jsmn.obj: /Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h
+
+../src/MSP430_GUI/jsmn/jsmn.c:
+
+../src/MSP430_GUI/jsmn/jsmn.h:
+
+/Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stddef.h:
+
+/Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h:
+
+/Applications/ti/ccs2041/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h:
+
