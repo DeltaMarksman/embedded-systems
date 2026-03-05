651e64367fe0af3e79ade3015a050d83ffbd36a0
unknown <danielb2lego@gmail.com>
2026-03-05 01:40:47 -0500

    Completes part 3.

--- lab_5/main.c
+++ lab_5/main.c
@@ -23,6 +23,14 @@
 #define EXCL_MEM       LCDM3
 #define EXCL_BIT       BIT0
 
+int hours       = 0;
+int minutes     = 0;
+int seconds     = 0;
+int running     = 1;
+int s1_pressed  = 0;
+int s2_pressed  = 0;
+
+
 void Initialize_LCD();
 
 // Digit segment shapes for MSP430FR6989 LCD
@@ -39,24 +47,80 @@ const unsigned char LCD_Shapes[10] = {
     0xF7    // 9
 };
 
-void lcd_write_uint16(unsigned int n)
+
+void lcd_update_icons() {
+    if (running) {
+        TIMER_SYMBOL_MEM |= TIMER_SYMBOL_BIT;     // chronometer logo pn
+        EXCL_MEM   &= ~EXCL_BIT;      // exclamation off
+        COLON_MEM ^= COLON_BIT; // blink colon
+
+    } else {
+        TIMER_SYMBOL_MEM &= ~TIMER_SYMBOL_BIT;    // chronometer logo off
+        EXCL_MEM   |= EXCL_BIT;       // exclamation on
+        COLON_MEM  |= COLON_BIT;     // colon on when stopped
+    }
+}
+
+// handle overflow and underflow
+void time_normalize(void) {
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+    } else if (seconds < 0) {
+        seconds = 59;
+        minutes--;
+    }
+
+    if (minutes >= 60) {
+        minutes = 0;
+        hours++;
+    } else if (minutes < 0) {
+        minutes = 59;
+        hours--;
+    }
+
+    if (hours >= 12) {
+        hours = 0;
+    } else if (hours < 0) {
+        hours = 11;
+    }
+}
+
+void lcd_write_time(int h, int m, int s) {
+
+    unsigned int d1 = h / 10;
+    unsigned int d2 = h % 10;
+    unsigned int d3 = m / 10;
+    unsigned int d4 = m % 10;
+    unsigned int d5 = s / 10;
+    unsigned int d6 = s % 10;
+
+    LCD_DIGIT_1 = LCD_Shapes[d1];
+    LCD_DIGIT_2 = LCD_Shapes[d2];
+    LCD_DIGIT_3 = LCD_Shapes[d3];
+    LCD_DIGIT_4 = LCD_Shapes[d4];
+    LCD_DIGIT_5 = LCD_Shapes[d5];
+    LCD_DIGIT_6 = LCD_Shapes[d6];
+}
+
+void lcd_update() {
+    lcd_write_time(hours, minutes, seconds);
+    lcd_update_icons();
+}
+
+void time_increment_one_second(void)
 {
-    // find each digit
-    unsigned int d1 = (n / 100000) % 10;
-    unsigned int d2 = (n / 10000)  % 10;
-    unsigned int d3 = (n / 1000)   % 10;
-    unsigned int d4 = (n / 100)    % 10;
-    unsigned int d5 = (n / 10)     % 10;
-    unsigned int d6 = (n / 1)      % 10;
+    seconds++;
+    time_normalize();
+}
 
-    // set ctrl regs to digits. If not needed, clear.
-    LCD_DIGIT_1 = (n < 100000) ? 0 : LCD_Shapes[d1];
-    LCD_DIGIT_2 = (n < 10000)  ? 0 : LCD_Shapes[d2];
-    LCD_DIGIT_3 = (n < 1000)   ? 0 : LCD_Shapes[d3];
-    LCD_DIGIT_4 = (n < 100)    ? 0 : LCD_Shapes[d4];
-    LCD_DIGIT_5 = (n < 10)     ? 0 : LCD_Shapes[d5];
-    LCD_DIGIT_6 = LCD_Shapes[d6]; // if the nubmer is 0 we still want to show 0
+void time_fast_forward_step(void) {
+    time_increment_one_second();
+}
 
+void time_rewind_step(void) {
+    seconds--;
+    time_normalize();
 }
 
 void config_ACLK_to_32KHz_crystal() {
@@ -80,10 +144,7 @@ void config_ACLK_to_32KHz_crystal() {
 }
 
 
-int counter = 0;
-
-
-int main(void) {
+    int main(void) {
 
     volatile unsigned int n;
 
@@ -115,12 +176,52 @@ int main(void) {
 
     // Initialize LCD
     Initialize_LCD();
+    lcd_update();
+    LCDM20 = BIT0; // decimal point
 
 
-    // Flash the red LED forever
     for (;;) {
-        for (n = 0; n <= 60000; n++) {}   // Delay
-        P1OUT ^= redLED;                  // Toggle LED
+
+        // pause and reset
+        if (s1_pressed) {
+            s1_pressed = 0;
+
+            // count time s1 is pressed
+            unsigned int count = 0;
+            while (!(P1IN & S1_BUTTON)) {   // still pressed (active low)
+                __delay_cycles(20000);      // debounce
+                count++;
+                if (count > 150) {          // long press threshold
+                    // long press for reset and stop
+                    running = 0;
+                    hours = minutes = seconds = 0;
+                    lcd_update();
+                    break;
+                }
+            }
+
+            if (count <= 150) {
+                // short press pause
+                running = !running;
+                lcd_update();
+            }
+        }
+
+        // fast forward and rewind
+        if (s2_pressed) {
+            while (!(P1IN & S2_BUTTON)) {
+                if (!(P1IN & S1_BUTTON)) {
+                    time_rewind_step();
+                } else {
+                    time_fast_forward_step();
+                }
+                lcd_update();
+
+                __delay_cycles(2000);
+            }
+
+            s2_pressed = 0;
+        }
     }
 
     return 0;
@@ -130,26 +231,26 @@ int main(void) {
 #pragma vector = TIMER0_A0_VECTOR
 __interrupt void TIMER0_A0_ISR(void)
 {
-    counter++;
-    lcd_write_uint16(counter);
+    if (running) {
+        time_increment_one_second();
+        lcd_update();
+    }
 }
 
-// clearing and adding
 #pragma vector = PORT1_VECTOR
 __interrupt void PORT1_ISR(void)
 {
-    if (P1IFG & BIT1) {       // S1 pressed
-        _delay_cycles(20000);
-        counter = 0;
-        lcd_write_uint16(counter);
-        P1IFG &= ~BIT1;
+    if (P1IFG & S1_BUTTON) {       // S1 pressed (falling edge)
+        if (s2_pressed == 0)
+            s1_pressed = 1;
+        __delay_cycles(20000);      // debounce
+        P1IFG &= ~S1_BUTTON;
     }
 
-    if (P1IFG & BIT2) {       // S2 pressed
-        _delay_cycles(20000);
-        counter += 1000;
-        lcd_write_uint16(counter);
-        P1IFG &= ~BIT2;
+    if (P1IFG & S2_BUTTON) {       // S2 pressed (falling edge)
+        s2_pressed = 1;
+        __delay_cycles(20000);      // debounce
+        P1IFG &= ~S2_BUTTON;
     }
 }
 
